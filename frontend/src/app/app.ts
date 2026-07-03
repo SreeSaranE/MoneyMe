bf37e3f21b7a02e6b77db95cff620537f10e63b5
SreeSaranE <sreesaran1007@gmail.com>
2026-07-03 19:01:19 +0530

    chore(frontend): initialize frontend project

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './services/theme/theme-service';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('frontend');
+
+  constructor(
+    private readonly themeService: ThemeService
+  ){}
+}
